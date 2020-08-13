f7e997a8c186c1f91b6a817c6ad0911f4593b49c
Visaj <kapadiavisaj@gmail.com>
2020-08-12 19:44:54 -0700

    second

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-fosdfi
+MY_INSTANCE_NAME="python-postgres-connection"
+ZONE=us-central1-a
+
+gcloud compute instances create $MY_INSTANCE_NAME \
+    --image-family=debian-9 \
+    --image-project=debian-cloud \
+    --machine-type=g1-small \
+    --scopes userinfo-email,cloud-platform \
+    --metadata-from-file startup-script=startup-script.sh \
+    --zone $ZONE \
+    --tags http-server
